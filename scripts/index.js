4835b2d1313922193ed69c362a2a67020fa55422
Kaisser Anthony Sereno <serenokaisser@gmail.com>
2024-09-03 17:03:38 +0800

    Working close button and event listener added

--- scripts/index.js
+++ scripts/index.js
@@ -19,9 +19,14 @@ const addItem = () => {
     let li = document.createElement("li"); // used to create an li element in the DOM
     let checkBox = document.createElement("input"); //creating an input element
     checkBox.type = "checkbox"; // creating a type checkbox from the created input element
+    checkBox.className = "check-box";
+
+    let closeButton = document.createElement("button"); //creating a close button
+    closeButton.textContent = "X";
 
     li.appendChild(checkBox); // this will add the checkbox created
     li.appendChild(text); // this will create the text node within the li element
+    li.appendChild(closeButton); // this will create the close button after the text node
     itemList.appendChild(li); // the li element containing the text node will be added to the ul
 
     // Eventlistener for the checkbox
@@ -33,20 +38,12 @@ const addItem = () => {
         }
     })
 
+    //Eventlistener for the closeButton
+    closeButton.addEventListener("click", () => {
+        itemList.removeChild(li);
+    })
+
     inputField.value = ""; // resetting the input bar after entering the item
-
-    let span = document.createElement("span"); // creates a span element
-    var txt = document.createTextNode("\u00D7"); // creates a text node with "X" symbol
-    span.className = "close"; // assigning a class name to the span
-    span.appendChild(txt); // appending the "X" symbol to the span
-    li.appendChild(span); // the span is added to the li element
-
-    for(i = 0; i < close.length; i++){
-        close[i].onclick = () => {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
 
 }
 
